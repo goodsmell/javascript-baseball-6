d82fd1bb11bf5cd4a974abdda0acd96d7ffd3127
goodsmell <87801306+goodsmell@users.noreply.github.com>
2024-10-15 11:03:30 +0900

    feat : 시작 문구 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    //1. 게임 시작
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+  }
+
+ 
 }
 
 export default App;
